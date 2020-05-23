ba897b40e942c1a2e9b73cb0a4ccce6ea203db90
Alexander Minchev <aleksander.minchev18@gmail.com>
2020-05-23 12:15:46 +0200

    SQL again

--- apartmentsSQL.sql
+++ apartmentsSQL.sql
@@ -5,6 +5,7 @@ USE apartmentsdatabase;
 CREATE TABLE apartments
 (
 apID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+address VARCHAR(45) NOT NULL,
 zip VARCHAR(5) NOT NULL,
 rent INT NOT NULL,
 size INT NOT NULL,
@@ -13,7 +14,7 @@ has_furniture TINYINT NOT NULL
 );
 
 INSERT INTO apartments VALUES
-(3532,2400,3000,40,4,true),
-(4503,2000,4000,25,7,true),
-(4509,5000,5000,45,2,false),
-(4510,2800,10000,100,5,false);
+(1,"Emdrup 45",2400,3000,40,4,true),
+(2,"Nansensgade 19",2000,4000,25,7,true),
+(3,"Enighedsvej 52",5000,5000,45,2,false),
+(4,"Kulsvierparken A09",2800,10000,100,5,false);
